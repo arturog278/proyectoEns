f62405f0c6e8c29e00a026eaef58d84cc1071a94
v <sebasvap20@gmail.com>
2016-08-29 18:44:48 -0500

    :C

--- src/mx/uaemex/fi/ens/Fase_1.java
+++ src/mx/uaemex/fi/ens/Fase_1.java
@@ -6,6 +6,10 @@ public class Fase_1 {
 
 	public static void main(String args[]) {
         try{
+        	File temp= File.createTempFile("ArchivoTemp",".tmp" );
+        	temp.deleteOnExit();//Archivo temporal para guardar lo boenido
+        	BufferedWriter bw = new BufferedWriter(new FileWriter(temp));
+        	
         	System.out.println ("Por favor introduzca el nombre del programa a traducir (con extension .asm):");
             String entradaTeclado = "";
             Scanner entradaEscaner = new Scanner (System.in); 
@@ -15,29 +19,71 @@ public class Fase_1 {
             BufferedReader buffer = new BufferedReader(new InputStreamReader(entrada));
             String strLinea;
             while ((strLinea = buffer.readLine()) != null)   {
-            	if (strLinea.contains(";"))
+            	if (!strLinea.isEmpty())
             	{
-            		
-					int i;
-					i=0;
-					while(strLinea.charAt(i)==';'){
-            			i=i+1;
-            		}
-					if(strLinea.charAt(0)==';'){
-						strLinea="";
-					}
-					else{
-						strLinea=strLinea.substring(0, i);
-					}
-					
-					
-            			
-            	}
 
-                System.out.println (strLinea);
+            			
+            		if (strLinea.contains(";"))
+                	{
+                		
+    					int i = 0;    				
+    					int b = 0;
+    					while(strLinea.charAt(i)!=';'){
+                			i=i+1;
+							if(strLinea.charAt(i)!=' '){
+                				b=1;
+                				break;
+                		}
+    					if(strLinea.charAt(0)!=';' && b==1){
+    						strLinea=strLinea.substring(0, i);
+    						if(strLinea.charAt(0)==' ')
+                    		{
+                    			while(strLinea.charAt(0)==' '){
+                    				strLinea=strLinea.substring(1);
+                    			}
+                    		}
+    						}
+    						
+    						
+    						//bw.write (strLinea+"\n");
+    					}
+    					
+    					
+                			
+                	}
+                	else{
+                		
+                		if(strLinea.charAt(0)==' ')
+                		{
+                			while(strLinea.charAt(0)==' '){
+                				strLinea=strLinea.substring(1);
+                			}
+                		}
+                		
+                		System.out.println(strLinea);
+                	}
+            	}
+            	
+                
 
             }
             entrada.close();
+            /*
+            bw.close();
+            BufferedReader bufferTemp = new BufferedReader(new FileReader(temp));
+            while ((strLinea = bufferTemp.readLine()) != null)   {
+            	
+            	if (strLinea.contains("data segment") || strLinea.contains("code segment")|| strLinea.contains("stack segment")){
+            		System.out.println(strLinea);
+            	}
+            	else
+            	{
+            		
+            	}
+            			
+            	}
+            */
+            
         }catch (Exception e){
             System.err.println("Ocurrio un error: " + e.getMessage());
         }
