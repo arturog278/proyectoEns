d39254f59aca0eb05ff73201ef5b4712e0e05e2e
Arturo <arturog278@gmail.com>
2016-08-28 23:28:40 -0500

    Modificandor archivo asm

--- plantillaEXE.asm
+++ plantillaEXE.asm
@@ -5,7 +5,7 @@ data segment
     pkey db "press any key...$"
     var1 db 'hola'
     var2 dw 0
-    
+
 ends
 
 stack segment
@@ -20,18 +20,17 @@ start:
     mov es, ax
 
     ; add your code here
-            
+
     lea dx, pkey
     mov ah, 9
     int 21h        ; output string at ds:dx
-    
-    ; wait for any key....    
+
+    ; wait for any key....
     mov ah, 1
     int 21h
-    
+
     mov ah,[di]
-    
+
     mov ax, 4c00h ; exit to operating system.
-    int 21h    
+    int 21h
 ends
-as
