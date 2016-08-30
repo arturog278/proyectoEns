74e8c494762c30b113950cb7aeb8dc551d9d4dce
Arturo <arturog278@gmail.com>
2016-08-29 22:47:55 -0500

    Fase 1 completada

--- src/mx/uaemex/fi/ens/Fase_1.java
+++ src/mx/uaemex/fi/ens/Fase_1.java
@@ -50,27 +50,85 @@ public class Fase_1 {
                 			
                 	}
                 	
-            	}
-            	
-                
-
-            
+            	}         
             entrada.close();
-            /*
             bw.close();
             BufferedReader bufferTemp = new BufferedReader(new FileReader(temp));
-            while ((strLinea = bufferTemp.readLine()) != null)   {
-            	
+            while ((strLinea = bufferTemp.readLine()) != null)   {           	
             	if (strLinea.contains("data segment") || strLinea.contains("code segment")|| strLinea.contains("stack segment")){
             		System.out.println(strLinea);
-            	}
-            	else
-            	{
+            	}else{
+            		String substr = "";
+            		int y=0;
+            		for (int x=0;x<strLinea.length();x++){
+            			switch(strLinea.charAt(x)){
+            			case '\"':
+            				y=x+1;
+            				while(strLinea.charAt(y)!='\"'){
+            					y+=1;
+            				}
+            				substr = strLinea.substring(x, y+1);
+            				System.out.println(substr);
+            				x=y;
+            				break;
+            			case '[':
+            				y=x+1;
+            				while(strLinea.charAt(y)!=']'){
+            					y+=1;
+            				}
+            				substr = strLinea.substring(x, y+1);
+            				System.out.println(substr);
+            				x=y;
+            				break;
+            			case '\'':
+            				y=x+1;
+            				while(strLinea.charAt(y)!='\''){
+            					y+=1;
+            				}
+            				substr = strLinea.substring(x, y+1);
+            				System.out.println(substr);
+            				x=y;
+            				break;
+            			default:
+            				if(strLinea.charAt(x)!=' ' & strLinea.charAt(x)!=','& strLinea.charAt(x)!=':'){
+            					if(strLinea.length()-x>3 && strLinea.substring(x, x+3)=="dub("){           				
+            						y=x+1;
+                       				while(strLinea.charAt(y)!=')'){
+                       					y+=1;
+                       				}
+                       				substr = strLinea.substring(x, y+1);
+                       				System.out.println(substr);
+                       				x=y;            						
+            					}else{
+            						y=x+1;
+            						if(y==strLinea.length()){
+            							System.out.println(strLinea.charAt(x));
+            						}else{
+            							while(strLinea.charAt(y)!=' ' & strLinea.charAt(x)!=',' & strLinea.charAt(x)!=':'){
+            								y+=1;
+            								if(y==strLinea.length()){
+            									y=y-1;
+            									break;
+            								}
+            							}
+            							substr = strLinea.substring(x, y+1);
+               							System.out.println(substr);
+               							x=y;   
+            						}			
+            					}
+            				}
+            				
+            			
+            				
+            			}
+            			
+            		}
+            	
+            	
             		
             	}
             			
             	}
-            */
             
         }catch (Exception e){
             System.err.println("Ocurrio un error: " + e.getMessage());
